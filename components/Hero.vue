cd0357f85dff570f11ee97172b9887229669c1ed
Raja s <rajaaltus@gmail.com>
2020-09-14 23:00:10 +0700

    initial commit

---
+++ components/Hero.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <img src="https://images.unsplash.com/photo-1589301773859-bb024d3ad558?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="Samosa">
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <Hero />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+
